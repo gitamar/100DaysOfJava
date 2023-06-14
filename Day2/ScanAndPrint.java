653ba8da3f87224f2e818d81cdac553b55a23639
Amarnath Thillai <gdgamarnath@gmail.com>
2023-06-14 03:07:21 +0000

    day 2

---
+++ Day2/ScanAndPrint.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class ScanAndPrint {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int firstInt = scanner.nextInt();
+        int secondInt = scanner.nextInt();
+        int thirdInt = scanner.nextInt();
+        
+        System.out.println(firstInt);
+        System.out.println(secondInt);
+        System.out.println(thirdInt);
+    }
+}
