5dac1c98b884e092a24b50a061b4cd5f91b260c7
Amarnath Thillai <gdgamarnath@gmail.com>
2023-06-16 03:18:02 +0000

    day 4

---
+++ Day4/StringFormat.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class StringFormat {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf( "%-15s%03d %n", s1, x);
+            }
+            System.out.println("================================");
+
+    }
+}
